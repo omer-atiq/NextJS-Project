32be19a360d77c3231ae019b2ef3980cbf4b3551
omer atiq <omer.attique@gmail.com>
2024-12-11 09:59:07 +0500

    added logotu logic

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,36 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+
+    const router = useRouter();
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout")
+            router.push('/login')
+
+        } catch (error: any) {
+
+            console.log(error)
+
+        }
+
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 text-4xl">
             <h1>Profile</h1>
             <hr></hr>
             <p>Profile page</p>
+
+            <hr />
+
+            <button onClick={logout} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+
+
+
         </div>
     )
 }
